97357edd57ddf212d408ab262269b91ec4d34523
unknown <hugocardosopb@gmail.com>
2022-09-27 20:34:53 -0300

    teste

---
+++ script.js
@@ -0,0 +1,8 @@
+let modal = document.getElementById('modal')
+let fechar = document.getElementById('btn')
+
+
+
+fechar.addEventListener('click', () => {
+    let close = modal.style.display = 'none'
+})
